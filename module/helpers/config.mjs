351953534d8655947b8e829aa35879f4dbf1e666
DavidEvesPrimis <info@primissolutions.co.uk>
2024-07-22 20:40:30 +0100

    Initial Commit - Boilerplate by Asacolips

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const ARMOUR_ASTIR = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+ARMOUR_ASTIR.abilities = {
+  str: 'ARMOUR_ASTIR.Ability.Str.long',
+  dex: 'ARMOUR_ASTIR.Ability.Dex.long',
+  con: 'ARMOUR_ASTIR.Ability.Con.long',
+  int: 'ARMOUR_ASTIR.Ability.Int.long',
+  wis: 'ARMOUR_ASTIR.Ability.Wis.long',
+  cha: 'ARMOUR_ASTIR.Ability.Cha.long',
+};
+
+ARMOUR_ASTIR.abilityAbbreviations = {
+  str: 'ARMOUR_ASTIR.Ability.Str.abbr',
+  dex: 'ARMOUR_ASTIR.Ability.Dex.abbr',
+  con: 'ARMOUR_ASTIR.Ability.Con.abbr',
+  int: 'ARMOUR_ASTIR.Ability.Int.abbr',
+  wis: 'ARMOUR_ASTIR.Ability.Wis.abbr',
+  cha: 'ARMOUR_ASTIR.Ability.Cha.abbr',
+};
